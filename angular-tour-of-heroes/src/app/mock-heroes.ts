63b20bf553607fcef963128c614423a4c5a56f22
Tyler Krueger <tylerkrueger@Tylers-MBP.Home>
2022-11-05 13:23:58 -0500

    toying around

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+    { id: 12, name: 'Tyler' },
+    { id: 13, name: 'Rindy' },
+    { id: 14, name: 'Jeff' },
+    { id: 15, name: 'Brenda' },
+    { id: 16, name: 'Andrew' },
+    { id: 17, name: 'Bruiser' }
+]
